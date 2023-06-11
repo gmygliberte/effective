c18c3d98aee57436bb38b45ebce880ffdcacb737
gongmingye <gliberte@163.com>
2023-06-11 22:46:45 +0800

    master 1

---
+++ src/main/java/com/gmy/git/Test.java
@@ -0,0 +1,12 @@
+package com.gmy.git;
+
+/**
+ * @author gmy
+ * @date 2023/6/11 22:46
+ */
+public class Test {
+    public static void main(String[] args) {
+        int i = 10;
+        System.out.println(i);
+    }
+}
