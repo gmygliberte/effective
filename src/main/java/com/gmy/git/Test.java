fd38eb47711c934da10c4a93638b99af52a69c6d
gongmingye <gliberte@163.com>
2023-06-11 22:41:00 +0800

    second commit

---
+++ src/main/java/com/gmy/git/Test.java
@@ -0,0 +1,12 @@
+package com.gmy.git;
+
+/**
+ * @author gmy
+ * @date 2023/6/11 22:38
+ */
+public class Test {
+    public static void main(String[] args) {
+        int i = 5;
+        System.out.println(i);
+    }
+}
