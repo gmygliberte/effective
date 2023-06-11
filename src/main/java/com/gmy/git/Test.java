878d7e7ab53ca754a7ca5418cbad2eb7151c5d93
gmygliberte <66014651+gmygliberte@users.noreply.github.com>
2023-06-11 22:41:00 +0800

    master 3

--- src/main/java/com/gmy/git/Test.java
+++ src/main/java/com/gmy/git/Test.java
@@ -2,13 +2,11 @@ package com.gmy.git;
 
 /**
  * @author gmy
- * @date 2023/6/11 22:46
+ * @date 2023/6/11 22:38
  */
 public class Test {
     public static void main(String[] args) {
-        int i = 10;
+        int i = 5;
         System.out.println(i);
-        int j = 5;
-        System.out.println(j);
     }
 }
